691a4ce5ac397a3577be27e969f50e7fcd6f52e3
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-09 17:13:51 -0500

    created layout and button functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,17 @@
-console.log("hello there");
 
 $(() => {
 
+  // Variables
+  const $button1 = $('#button1');
+  const $button2 = $('#button2');
 
+  $button1.on('click', () => {
+    console.log('button 1 was clicked');
+  })
 
-
+  $button2.on('click', () => {
+    console.log('button 2 was clicked');
+  })
 
 
 
