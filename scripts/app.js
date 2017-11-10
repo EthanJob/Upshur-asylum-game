b2f95b1011ecedb8e295ed5c745e727985f4a919
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-10 16:06:31 -0500

    added animation to title screen

--- scripts/app.js
+++ scripts/app.js
@@ -26,13 +26,16 @@ $(() => {
   const $chapter3Title = $('<h2>').text('Chapter 3').addClass('chapterTitle');
   // Rooms
   const $room1Title = $('<h3>').text('Room 1').addClass('roomTitle');
-  const $room2Title = $('<h3>').text('Room 2').addClass('roomTitle');
+  const $room2aTitle = $('<h3>').text('Room 2a').addClass('roomTitle');
+    const $room2bTitle = $('<h3>').text('Room 2b').addClass('roomTitle');
   const $room3Title = $('<h3>').text('Room 3').addClass('roomTitle');
   const $enemyRoom = $('<h3>').text('ENEMY').addClass('roomTitle');
   // Story Paragraphs
   const $room1Text = $('<p>').html('The glass door just ahead of you is locked. <br><br>Path 1: There is a closed door to left. It looks like it may lead to a restroom. <br><br>Path 2: The door to the right is open and has light coming from inside. <br><br>Which path will you take?').addClass('roomText');
 
-  const $room2Text = $('<p>').html('changed to story paragraph 2.').addClass('roomText');
+  const $room2aText = $('<p>').html('entered restroom.').addClass('roomText');
+
+  const $room2bText = $('<p>').html('entered room with light.').addClass('roomText');
 
   const $room3Text = $('<p>').html('changing to the third story paragraph.').addClass('roomText');
 
@@ -64,16 +67,16 @@ $(() => {
     console.log('button 1 was clicked');
     $clear();
     // change button id or class here
-    $room.append($room2Title);
-    $story.append($room2Text);
+    $room.append($room2aTitle);
+    $story.append($room2aText);
   })
 
   $button2.on('click', (event) => {
     console.log('button 2 was clicked');
     $clear();
     // change button id or class here
-    $room.append($room2Title);
-    $story.append($room2Text);
+    $room.append($room2bTitle);
+    $story.append($room2bText);
   })
 
 
