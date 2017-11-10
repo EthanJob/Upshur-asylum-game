f364ca7adbbf702c2a85df46ab43a70304594c1b
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-09 23:19:24 -0500

    created start screen and chpter 1 outline

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,35 @@
 $(() => {
 
   // Variables
-  const $button1 = $('#button1');
-  const $button2 = $('#button2');
+  const $container = $('#container');
+  const $buttonArea = $('.buttonArea');
+  const $healthArea = $('.health');
+  const $health100 = $('<h3>').text('Health: 100%');
+  const $chapter = $('.chapter');
+  const $button1 = $('<button>').attr('id', 'button1').text('Path 1');
+  const $button2 = $('<button>').attr('id', 'button2').text('Path 2');
+  const $startBtn = $('#startButton');
 
-  $button1.on('click', () => {
+
+  // Buttons
+  $startBtn.on('click', (event) => {
+    console.log('clicked start button');
+    // removing start screen
+    $('.startTitle').removeClass();
+    $('.startInfo').remove();
+    $startBtn.remove();
+    // adding chapter 1
+    $chapter.append( $('<h2>Chapter 1<h2>') );
+    // append paragraph here
+    $buttonArea.append($button1, $button2);
+    $healthArea.append($health100);
+  })
+
+  $button1.on('click', (event) => {
     console.log('button 1 was clicked');
   })
 
-  $button2.on('click', () => {
+  $button2.on('click', (event) => {
     console.log('button 2 was clicked');
   })
 
