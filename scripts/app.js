ec61cc5d10b63a3e5d880b1f4830f4f5a5ef9fe3
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-10 12:08:18 -0500

    added first few text variables and organized code

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,53 @@
 
 $(() => {
 
-  // Variables
+  // VARIABLES
   const $container = $('#container');
   const $buttonArea = $('.buttonArea');
   const $healthArea = $('.health');
   const $health100 = $('<h3>').text('Health: 100%');
   const $chapter = $('.chapter');
+  const $room = $('.room');
+  const $main = $('.main');
+  // ACTION BUTTONS
   const $button1 = $('<button>').attr('id', 'button1').text('Path 1');
   const $button2 = $('<button>').attr('id', 'button2').text('Path 2');
+  const $attackBtn = $('<button>').attr('id', 'attack').text('Attack');
+  const $runBtn = $('<button>').attr('id', 'run').text('Run');
   const $startBtn = $('#startButton');
+  // TEXT VARIABLES
+  const $chapter1Txt = $('<h2>').text('Chapter 1');
+  const $chapter2Txt = $('<h2>').text('Chapter 2');
+  const $chapter3Txt = $('<h2>').text('Chapter 3');
+  const $room1Txt = $('<h3>').text('Room 1');
+  const $room2Txt = $('<h3>').text('Room 2');
+  const $room3Txt = $('<h3>').text('Room 3');
+
+  const $clear = () => {
+    $('p').remove();
+  }
 
 
-  // Buttons
+  // START BUTTON
   $startBtn.on('click', (event) => {
     console.log('clicked start button');
-    // removing start screen
+    // REMOVING START SCREEN
     $('.startTitle').removeClass();
     $('.startInfo').remove();
     $startBtn.remove();
-    // adding chapter 1
-    $chapter.append( $('<h2>Chapter 1<h2>') );
-    // append paragraph here
+    // ADDING CHAPTER 1
+    $chapter.append($chapter1Txt);
+    $room.append($room1Txt);
+    $main.append( $('<p>story for room 1 goes here.</p>') );
     $buttonArea.append($button1, $button2);
     $healthArea.append($health100);
+
   })
 
+  // PATH BUTTONS
   $button1.on('click', (event) => {
     console.log('button 1 was clicked');
+    $clear();
   })
 
   $button2.on('click', (event) => {
