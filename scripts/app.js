125bca3eb19610b6f956eea11fe19211392301ac
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-10 12:56:54 -0500

    fixed clear function

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,14 @@ $(() => {
   const $container = $('#container');
   const $buttonArea = $('.buttonArea');
   const $healthArea = $('.health');
-  const $health100 = $('<h3>').text('Health: 100%');
   const $chapter = $('.chapter');
   const $room = $('.room');
   const $main = $('.main');
+  // HEALTH BARS
+  const $health100 = $('<h3>').text('Health: 100%');
+  const $health75 = $('<h3>').text('Health: 75%');
+  const $health50 = $('<h3>').text('Health: 50%');
+  const $health25 = $('<h3>').text('Health: 25%');
   // ACTION BUTTONS
   const $button1 = $('<button>').attr('id', 'button1').text('Path 1');
   const $button2 = $('<button>').attr('id', 'button2').text('Path 2');
@@ -16,16 +20,21 @@ $(() => {
   const $runBtn = $('<button>').attr('id', 'run').text('Run');
   const $startBtn = $('#startButton');
   // TEXT VARIABLES
-  const $chapter1Txt = $('<h2>').text('Chapter 1');
-  const $chapter2Txt = $('<h2>').text('Chapter 2');
-  const $chapter3Txt = $('<h2>').text('Chapter 3');
-  const $room1Txt = $('<h3>').text('Room 1');
-  const $room2Txt = $('<h3>').text('Room 2');
-  const $room3Txt = $('<h3>').text('Room 3');
+  // Chapters
+  const $chapter1Title = $('<h2>').text('Chapter 1').addClass('chapterTitle');
+  const $chapter2Title = $('<h2>').text('Chapter 2').addClass('chapterTitle');
+  const $chapter3Title = $('<h2>').text('Chapter 3').addClass('chapterTitle');
+  // Rooms
+  const $room1Title = $('<h3>').text('Room 1').addClass('roomTitle');
+  const $room2Title = $('<h3>').text('Room 2').addClass('roomTitle');
+  const $room3Title = $('<h3>').text('Room 3').addClass('roomTitle');
+  // Story Paragraphs
+  const $room1Text = $('<p>').text('story for room 1 goes here.').addClass('roomText');
 
-  const $clear = () => {
-    $('p').remove();
-  }
+  const $clear = (event) => {
+    $('.roomTitle').remove();
+    $('.roomText').remove();
+  };
 
 
   // START BUTTON
@@ -36,22 +45,25 @@ $(() => {
     $('.startInfo').remove();
     $startBtn.remove();
     // ADDING CHAPTER 1
-    $chapter.append($chapter1Txt);
-    $room.append($room1Txt);
-    $main.append( $('<p>story for room 1 goes here.</p>') );
+    $chapter.append($chapter1Title);
+    $room.append($room1Title);
+    $main.append($room1Text);
     $buttonArea.append($button1, $button2);
     $healthArea.append($health100);
-
   })
 
   // PATH BUTTONS
   $button1.on('click', (event) => {
     console.log('button 1 was clicked');
     $clear();
+    // change button id here
+
   })
 
   $button2.on('click', (event) => {
     console.log('button 2 was clicked');
+    $clear();
+    // change button id here
   })
 
 
