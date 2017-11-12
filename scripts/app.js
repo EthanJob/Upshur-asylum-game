7718a742cab45f3df42edb7701b1ed3b8a19ae51
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-11 23:08:36 -0500

    added button appending for new screens

--- scripts/app.js
+++ scripts/app.js
@@ -13,12 +13,20 @@ $(() => {
   const $health75 = $('<h3>').text('Health: 75%').addClass('health75');
   const $health50 = $('<h3>').text('Health: 50%').addClass('health50');
   const $health25 = $('<h3>').text('Health: 25%').addClass('health25');
-  // ACTION BUTTONS
+  // BUTTONS
   const $startBtn = $('#startButton');
-  const $button1 = $('<button>').attr('id', 'button1').text('Path 1');
-  const $button2 = $('<button>').attr('id', 'button2').text('Path 2');
-  const $attackBtn = $('<button>').attr('id', 'attack').text('Attack');
-  const $runBtn = $('<button>').attr('id', 'run').text('Run');
+  // room 1
+  const $button1a = $('<button>').addClass('button1').text('Path 1');
+  const $button1b = $('<button>').addClass('button2').text('Path 2');
+  // room 2a
+  const $button2aa = $('<button>').addClass('button1').text('Path 1');
+  const $button2ab = $('<button>').addClass('button2').text('Path 2');
+  // room 2b
+  const $button2ba = $('<button>').addClass('button1').text('Path 1');
+  const $button2bb = $('<button>').addClass('button2').text('Path 2');
+  // attack/run buttons
+  const $attackBtn = $('<button>').addClass('button1').text('Attack');
+  const $runBtn = $('<button>').addClass('button2').text('Run');
   // TEXT VARIABLES
   // Chapters
   const $chapter1Title = $('<h2>').text('Chapter 1').addClass('chapterTitle');
@@ -33,16 +41,19 @@ $(() => {
   // Story Paragraphs
   const $room1Text = $('<p>').html('The glass door just ahead of you is locked. <br><br>Path 1: There is a closed door to left. It looks like it may lead to a restroom. <br><br>Path 2: The door to the right is open and has light coming from inside. <br><br>Which path will you take?').addClass('roomText');
 
-  const $room2aText = $('<p>').html('entered restroom.').addClass('roomText');
+  const $room2aText = $('<p>').html('entered restroom. *monster*').addClass('roomText');
 
   const $room2bText = $('<p>').html('entered room with light.').addClass('roomText');
 
   const $room3Text = $('<p>').html('changing to the third story paragraph.').addClass('roomText');
 
 
+  // FUNCTIONS
   const $clear = (event) => {
     $('.roomTitle').remove();
     $('.roomText').remove();
+    $('.button1').remove();
+    $('.button2').remove();
   };
 
 
@@ -57,27 +68,27 @@ $(() => {
     $chapter.append($chapter1Title);
     $room.append($room1Title);
     $story.append($room1Text);
-    $buttonArea.append($button1, $button2);
+    $buttonArea.append($button1a, $button1b);
     $healthArea.append($health100);
     $('body').attr('background', 'images/2door-hallway.jpg');
-  })
+  });
 
   // ROOM 1: PATH BUTTONS
-  $button1.on('click', (event) => {
+  $button1a.on('click', (event) => {
     console.log('button 1 was clicked');
     $clear();
-    // change button id or class here
     $room.append($room2aTitle);
     $story.append($room2aText);
-  })
+    $buttonArea.append($attackBtn, $runBtn);
+  });
 
-  $button2.on('click', (event) => {
+  $button1b.on('click', (event) => {
     console.log('button 2 was clicked');
     $clear();
-    // change button id or class here
     $room.append($room2bTitle);
     $story.append($room2bText);
-  })
+    $buttonArea.append($button2ba, $button2bb);
+  });
 
 
 
