c5b7f001048c754500fad02683d26b0dfb9ff0ae
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-13 12:48:42 -0500

    cleaned up some code and added third room

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 
 $(() => {
 
+  // ENEMIES
+  const $enemy1 = {
+    health: 10
+  };
+  const $enemy2 = {
+    health: 10
+  };
+  const $enemy3 = {
+    health: 10
+  };
   // VARIABLES
   const $container = $('#container');
   const $buttonArea = $('.buttonArea');
@@ -22,8 +32,11 @@ $(() => {
   const $button2aa = $('<button>').addClass('button1').text('Path 1');
   const $button2ab = $('<button>').addClass('button2').text('Path 2');
   // room 2b
-  const $button2ba = $('<button>').addClass('button1').text('Path 1');
-  const $button2bb = $('<button>').addClass('button2').text('Path 2');
+  const $button2ba = $('<button>').addClass('button1').text('Explore');
+  const $button2bb = $('<button>').addClass('button2').text('Next Room');
+  // room 3
+  const $button3a = $('<button>').addClass('button1').text('Path 1');
+  const $button3b = $('<button>').addClass('button2').text('Path 2');
   // attack/run buttons
   const $attackBtn = $('<button>').addClass('button1').text('Attack');
   const $runBtn = $('<button>').addClass('button2').text('Run');
@@ -43,9 +56,9 @@ $(() => {
 
   const $room2aText = $('<p>').html('entered restroom. *monster*').addClass('roomText');
 
-  const $room2bText = $('<p>').html('entered room with light.').addClass('roomText');
+  const $room2bText = $('<p>').html("It looks like a kitchen with a stove-top and a fridge. There aren't any items on the tables and the room looks desolate. <br><br> Will you explore the fridge?<br><br> or <br><br> Will you continue to the next room?").addClass('roomText');
 
-  const $room3Text = $('<p>').html('changing to the third story paragraph.').addClass('roomText');
+  const $room3Text = $('<p>').html('this is the third story paragraph.').addClass('roomText');
 
 
   // FUNCTIONS
@@ -56,7 +69,31 @@ $(() => {
     $('.button2').remove();
   };
 
-
+  // Chapters
+  const $chapter1 = (event) => {
+    $chapter.append($chapter1Title);
+    $room.append($room1Title);
+    $story.append($room1Text);
+    $buttonArea.append($button1a, $button1b);
+    $healthArea.append($health100);
+    $('body').attr('background', 'images/2door-hallway.jpg');
+  };
+  const $chapter2a = (event) => {
+    $room.append($room2aTitle);
+    $story.append($room2aText);
+    $buttonArea.append($attackBtn, $runBtn);
+  };
+  const $chapter2b = (event) => {
+    $room.append($room2bTitle);
+    $story.append($room2bText);
+    $buttonArea.append($button2ba, $button2bb);
+  };
+  const $chapter3 = (event) => {
+    $room.append($room3Title);
+    $story.append($room3Text);
+    $buttonArea.append($button3a, $button3b);
+    $('body').attr('background', 'images/cage-hallway.jpg');
+  };
   // START BUTTON
   $startBtn.on('click', (event) => {
     console.log('clicked start button');
@@ -65,34 +102,37 @@ $(() => {
     $('.startInfo').remove();
     $startBtn.remove();
     // ADDING CHAPTER 1
-    $chapter.append($chapter1Title);
-    $room.append($room1Title);
-    $story.append($room1Text);
-    $buttonArea.append($button1a, $button1b);
-    $healthArea.append($health100);
-    $('body').attr('background', 'images/2door-hallway.jpg');
+    $chapter1();
   });
 
   // ROOM 1: PATH BUTTONS
   $button1a.on('click', (event) => {
     console.log('button 1 was clicked');
     $clear();
-    $room.append($room2aTitle);
-    $story.append($room2aText);
-    $buttonArea.append($attackBtn, $runBtn);
+    $chapter2a();
   });
 
   $button1b.on('click', (event) => {
     console.log('button 2 was clicked');
     $clear();
-    $room.append($room2bTitle);
-    $story.append($room2bText);
-    $buttonArea.append($button2ba, $button2bb);
+    $chapter2b();
   });
 
+  // ROOM 2A: PATH BUTTONS
+  $attackBtn.on('click', (event) => {
+    console.log("attack button clicked");
+    $clear();
+    // Attack sequence here with an if...else function
+    $chapter3();
+  });
 
+  $runBtn.on('click', (event) => {
+    console.log("run button clicked");
+    $clear();
+    $chapter3();
+  });
 
-
-
+  // ROOM 2B: PATH BUTTONS
+  
 
 }); // End
