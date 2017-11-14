297a757beb15d11a19216274959ae3caa4ad5415
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-11-14 14:54:57 -0500

    finished chapter 2 room 2 monster layout

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,7 @@ $(() => {
   // ITEMS
   const items = {
     keys: null,
+    doorKeys: null,
     weapons: null
   };
 
@@ -37,6 +38,8 @@ $(() => {
   const $startBtn = $('#startButton');
   const $cont1 = $('<button>').attr('id', 'startButton').text('Next Room');
   const $cont2 = $('<button>').attr('id', 'startButton').text('Next Room');
+  const $cont3 = $('<button>').attr('id', 'startButton').text('Continue');
+  const $cont4 = $('<button>').attr('id', 'startButton').text('Continue');
   // room 1
   const $button1a = $('<button>').addClass('button1').text('Path 1');
   const $button1b = $('<button>').addClass('button2').text('Path 2');
@@ -60,6 +63,12 @@ $(() => {
   // attack/run buttons
   const $attackBtn = $('<button>').addClass('button1').text('Attack');
   const $runBtn = $('<button>').addClass('button2').text('Run');
+  // Chapter 2
+  const $attackBtn2 = $('<button>').addClass('button1').text('Attack');
+  const $runBtn2 = $('<button>').addClass('button2').text('Run');
+  // room 2b
+  const $attackBtn3 = $('<button>').addClass('button1').text('Attack');
+  const $runBtn3 = $('<button>').addClass('button2').text('Run');
   // TEXT VARIABLES
   // Chapters
   const $chapter1Title = $('<h2>').text('Chapter 1').addClass('chapterTitle');
@@ -68,9 +77,10 @@ $(() => {
   // Rooms
   const $room1Title = $('<h3>').text('Room 1').addClass('roomTitle');
   const $room2aTitle = $('<h3>').text('Room 2a').addClass('roomTitle');
-    const $room2bTitle = $('<h3>').text('Room 2b').addClass('roomTitle');
+  const $room2bTitle = $('<h3>').text('Room 2b').addClass('roomTitle');
   const $room3Title = $('<h3>').text('Room 3').addClass('roomTitle');
-  const $enemyRoom = $('<h3>').text('ENEMY').addClass('roomTitle');
+  const $enemyRoom = $('<h3>').text('Enemy').addClass('roomTitle');
+  const $chp2rm2Title = $('<h3>').text('Room 2').addClass('roomTitle');
   // Story Paragraphs
   const $room1Text = $('<p>').html('The glass door just ahead of you is locked. <br><br>Path 1: There is a closed door to left. It looks like it may lead to a restroom. <br><br>Path 2: The door to the right is open and has light coming from inside. <br><br>Which path will you take?').addClass('roomText');
 
@@ -80,7 +90,9 @@ $(() => {
 
   const $room3Text = $('<p>').html('The hallway in front of you has an open door to the left and an open door to the right. The left door is silent but has a dim light coming from it. The door to the right has is well-lit and has a dripping sound coming from it. idk fix this part.<br><br> Path 1: Enter the room to the left. <br><br> Or <br><br>Path 2: Enter the door to the right.').addClass('roomText');
 
-  const $chp2Txt = $('<p>').html("An old wheelchair lies on its back as blood covers the floor. There are two paths here: <br>A locked elevator to the left and a descending staircase to the right that may lead to a basement. <br><br> Will you unlock the elevator? <br><br> Or <br><br> Take the stairs to the basement?").addClass('roomText');
+  const $chp2Txt = $('<p>').html("An old wheelchair lies on its back as blood covers the floor. There are two paths here: <br>A locked elevator to the left and a descending staircase to the right that may lead to the sewers. <br><br> Will you use the elevator? <br><br> Or <br><br> Will you take the stairs to the sewers?").addClass('roomText');
+
+  const $chp2rm2Txt = $('<p>').html("monster will be here. *monster* <br><br> *monster health*").addClass('roomText');
 
   // FUNCTIONS
   const $clear = (event) => {
@@ -148,6 +160,17 @@ $(() => {
     $story.append($room3Text);
     $buttonArea.append($button3a, $button3b);
     $('body').attr('background', 'images/cage-hallway.jpg');
+  };
+  const $chp2rm2a = (event) => {
+    $room.append($chp2rm2Title);
+    $story.append($chp2rm2Txt);
+    $buttonArea.append($attackBtn2, $runBtn2);
+    $('body').attr('background', 'images/level2-hall.jpg');
+  };
+  const $chp2rm2b = (event) => {
+    $room.append($chp2rm2Title);
+    $story.append($chp2rm2Txt);
+    $('body').attr('background', 'images/sewer.jpg');
   };
 
   // START BUTTON
@@ -243,19 +266,70 @@ $(() => {
       items.keys = null;
       $chapter2Btn1.remove();
       $chapter2Btn2.remove();
-      $buttonArea.append($chapter2Btn1b);
-      $buttonArea.append($chapter2Btn2);
+      $buttonArea.append($chapter2Btn1b, $chapter2Btn2);
     } else {
       console.log('elevator is locked');
       $clear();
       $story.append( $('<p>').html("You don't have an elevator key!").addClass('roomText') );
       $buttonArea.append($backButton);
-    }
+    };
+    $backButton.on('click', (event) => {
+      $clear();
+      $backButton.remove();
+      $chapter2();
+    });
+    $chapter2Btn2.on('click', (event) => {
+      console.log('taking stairs');
+      $clear();
+      $chp2rm2b();
+      $buttonArea.append($attackBtn3, $runBtn3);
+    });
   });
-  $backButton.on('click', (event) => {
+  $chapter2Btn2.on('click', (event) => {
+    console.log('taking stairs');
     $clear();
-    $backButton.remove();
-    $chapter2();
+    $chp2rm2b();
+    $buttonArea.append($attackBtn3, $runBtn3);
+  });
+  $chapter2Btn1b.on('click', (event) => {
+    console.log('taking elevator');
+    $clear();
+    $chp2rm2a();
+    $buttonArea.append($attackBtn2, $runBtn2);
+  });
+  // ROOM 2
+  $attackBtn2.on('click', (event) => {
+    console.log('attacking');
+    $('.button1').remove();
+    $('.button2').remove();
+    $('.roomText').remove();
+    $story.append( $('<p>').addClass('roomText').html("You killed the monster!") );
+    $buttonArea.append($cont3);
+  });
+  $runBtn2.on('click', (event) => {
+    console.log('running');
+    $('.button1').remove();
+    $('.button2').remove();
+    $('.roomText').remove();
+    $story.append( $('<p>').addClass('roomText').html("You escaped the monster!") );
+    $buttonArea.append($cont3);
+  });
+  // SEWERS
+  $attackBtn3.on('click', (event) => {
+    console.log('attacking');
+    $('.button1').remove();
+    $('.button2').remove();
+    $('.roomText').remove();
+    $story.append( $('<p>').addClass('roomText').html("You killed the monster!") );
+    $buttonArea.append($cont4);
+  });
+  $runBtn3.on('click', (event) => {
+    console.log('running');
+    $('.button1').remove();
+    $('.button2').remove();
+    $('.roomText').remove();
+    $story.append( $('<p>').addClass('roomText').html("You escaped the monster!") );
+    $buttonArea.append($cont4);
   });
 
   console.log(items);
